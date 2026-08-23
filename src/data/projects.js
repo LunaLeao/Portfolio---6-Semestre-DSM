c613fe32670f35e487d123063611eadb1bb572a9
Luna Leão <lunaleao.maria@gmail.com>
2026-08-23 18:44:35 -0300

    Commit Inicial

---
+++ src/data/projects.js
@@ -0,0 +1,46 @@
+export const projectsData = [
+    {
+        id: 1,
+        title: "SafeEcotour",
+        description: "Plataforma web construída para divulgação dos serviços de turimos da SafeEcotour, apresentando os possíveis" + 
+        "passeios, informações básicas da jornada e a equipe responsável, assim como direcionando o usuário para o contato com a empresa.",
+        role: "UX/UI Design e Desenvolvimento Front-end",
+        technologies: ["HTML", "CSS", "JavaScript", "Framework Bootstrap", "Figma", "Github"],
+        link: "https://github.com/LunaLeao/PI1SemestreSafeEcoTour"
+    },
+    {
+        id: 2,
+        title: "AgroStock",
+        description: "Plataforma web dedicada a organização e gerenciamento de estoque para pequenos agricultores," + 
+        "assim como o registro de vendas, compras e o levantamento de dados do mês por meio de DashBoards e relatórios.",
+        role: "UX/UI Design e Desenvolvimento Front-end",
+        technologies: ["Node.js", "Handlebars", "Express", "Bcrypt", "MySQL", "Figma", "Github"],
+        link: "https://github.com/LunaLeao/PI-2Semestre-AgroStock"
+    },
+    {
+        id: 3,
+        title: "Portal Bruxa Acadêmica",
+        description: "Plataforma web construída para compartilhamento de conhecimentos sobre o mundo da Astrologia e Tarologia," +
+        "onde o usuário pode ler artigos, realizar testes de tiragem e criar seu Mapa Astral após o login na plataforma.",
+        role: "UX/UI Design, Desenvolvimento Front-end e Documentação Técnica",
+        technologies: ["React.js", "MongoDB", "Vercel", "Render", "Figma", "Github"],
+        link: "https://github.com/LunaLeao/portal-bruxa-academica"
+    },
+    {
+        id: 4,
+        title: "Pindorama",
+        description: "Plataforma web dedicada a documentar e compartilhar o patrimônio cultural brasileiro por meio de artigos," +
+        "além da divulgação de eventos culturais.",
+        role: "Desenvolvimento Front-end, Ilustrações e Documentação Técnica",
+        technologies: ["React", "Ruby on Rails", "Docker", "SASS", "Python", "PostgreSQL", "Neon", "Vercel", "Render", "Figma", "Github"],
+        link: "https://github.com/Debora-Carvalho/sistema-web-pindorama"
+    },
+    {
+        id: 5,
+        title: "Poupas",
+        description: "Aplicativo mobile com sistema de autenticação focado em gestão.",
+        role: "UX/UI Design, Ilustrações, Desenvolvimento Back-end, Integração de Autenticação e Criação do DER/MER.",
+        technologies: ["Flutter", "Dart", "GO", "C#", "PostgreSQL", "Neon", "Render", "Figma", "Github"],
+        link: "https://github.com/Best-of-the-class/Poupas"
+    }
+];
